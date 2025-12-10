2539038c59690bc863451bce80d9f549b0d1a1dd
Roman Bukin <r.bukin@dodobrands.io>
2025-12-10 18:07:07 +0300

    Update documentation to use stable WebAuthn specification links and improve comments readability

--- src/WebAuthn.Net/Models/Protocol/Json/RegistrationCeremony/CreateOptions/PublicKeyCredentialRpEntityJSON.cs
+++ src/WebAuthn.Net/Models/Protocol/Json/RegistrationCeremony/CreateOptions/PublicKeyCredentialRpEntityJSON.cs
@@ -8,16 +8,13 @@ namespace WebAuthn.Net.Models.Protocol.Json.RegistrationCeremony.CreateOptions;
 /// </summary>
 /// <remarks>
 ///     <para>
-///         <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#dictionary-user-credential-params">Web Authentication: An API for accessing Public Key Credentials Level 3 - §5.4.2. Relying Party Parameters for Credential Generation (dictionary PublicKeyCredentialRpEntity)</a>
+///         <a href="https://www.w3.org/TR/webauthn-3/#dictionary-rp-credential-params">Web Authentication: An API for accessing Public Key Credentials Level 3 - Relying Party Parameters for Credential Generation (dictionary PublicKeyCredentialRpEntity)</a>
 ///     </para>
 ///     <para>
-///         <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-parseCreationOptionsFromJSON">
-///             Web Authentication: An API for accessing Public Key Credentials Level 3 - §5.1.9. Deserialize Registration ceremony options - PublicKeyCredential's
-///             parseCreationOptionsFromJSON() Method
-///         </a>
+///         <a href="https://www.w3.org/TR/webauthn-3/#sctn-parseCreationOptionsFromJSON">Web Authentication: An API for accessing Public Key Credentials Level 3 - Deserialize Registration ceremony options - PublicKeyCredential’s parseCreationOptionsFromJSON() Method</a>
 ///     </para>
 ///     <para>
-///         <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#dictdef-publickeycredentialcreationoptions">Web Authentication: An API for accessing Public Key Credentials Level 3 - §5.4. Options for Credential Creation (dictionary PublicKeyCredentialCreationOptions)</a>
+///         <a href="https://www.w3.org/TR/webauthn-3/#dictionary-makecredentialoptions">Web Authentication: An API for accessing Public Key Credentials Level 3 - Options for Credential Creation (dictionary PublicKeyCredentialCreationOptions)</a>
 ///     </para>
 /// </remarks>
 // ReSharper disable once InconsistentNaming
@@ -26,8 +23,28 @@ public class PublicKeyCredentialRpEntityJSON
     /// <summary>
     ///     Constructs <see cref="PublicKeyCredentialRpEntityJSON" />.
     /// </summary>
-    /// <param name="name">A <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#human-palatability">human-palatable</a> identifier for the <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#relying-party">Relying Party</a>, intended only for display.</param>
-    /// <param name="id">A unique identifier for the <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#relying-party">Relying Party</a> entity, which sets the <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#rp-id">RP ID</a>.</param>
+    /// <param name="name">
+    ///     <para>
+    ///         [DEPRECATED] [REQUIRED] When inherited by <a href="https://www.w3.org/TR/webauthn-3/#dictdef-publickeycredentialrpentity">PublicKeyCredentialRpEntity</a> it is a <a href="https://www.w3.org/TR/webauthn-3/#human-palatability">human-palatable</a> identifier for the
+    ///         <a href="https://www.w3.org/TR/webauthn-3/#relying-party">Relying Party</a>, intended only for display.
+    ///     </para>
+    ///     <para>
+    ///         This member is deprecated because many <a href="https://www.w3.org/TR/webauthn-3/#client">clients</a> do not display it, but it remains a required dictionary member for backwards compatibility.
+    ///         <a href="https://www.w3.org/TR/webauthn-3/#relying-party">Relying Parties</a> MAY, as a safe default, set this equal to the <a href="https://www.w3.org/TR/webauthn-3/#rp-id">RP ID</a>.
+    ///     </para>
+    ///     <para>
+    ///         When storing a <a href="https://www.w3.org/TR/webauthn-3/#dom-publickeycredentialentity-name">"name"</a> member’s value, the value MAY be truncated as described in <a href="https://www.w3.org/TR/webauthn-3/#sctn-strings-truncation">"String Truncation"</a> using a size
+    ///         limit greater than or equal to 64 bytes.
+    ///     </para>
+    /// </param>
+    /// <param name="id">
+    ///     <para>A unique identifier for the <a href="https://www.w3.org/TR/webauthn-3/#relying-party">Relying Party</a> entity, which sets the <a href="https://www.w3.org/TR/webauthn-3/#rp-id">RP ID</a>.</para>
+    ///     <para>
+    ///         Specifies the <a href="https://www.w3.org/TR/webauthn-3/#rp-id">RP ID</a> the credential should be scoped to. If omitted, its value will be the <a href="https://www.w3.org/TR/credential-management-1/#credentialscontainer">CredentialsContainer</a> object’s
+    ///         <a href="https://html.spec.whatwg.org/multipage/webappapis.html#relevant-settings-object">relevant settings object’s</a> <a href="https://html.spec.whatwg.org/multipage/webappapis.html#concept-settings-object-origin">origin’s</a>
+    ///         <a href="https://html.spec.whatwg.org/multipage/browsers.html#concept-origin-effective-domain">effective domain</a>.
+    ///     </para>
+    /// </param>
     [JsonConstructor]
     public PublicKeyCredentialRpEntityJSON(string name, string? id)
     {
@@ -36,25 +53,21 @@ public class PublicKeyCredentialRpEntityJSON
     }
 
     /// <summary>
-    ///     A <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#human-palatability">human-palatable</a> identifier for the <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#relying-party">Relying Party</a>, intended only for display.
+    ///     <para>
+    ///         [DEPRECATED] [REQUIRED] When inherited by <a href="https://www.w3.org/TR/webauthn-3/#dictdef-publickeycredentialrpentity">PublicKeyCredentialRpEntity</a> it is a <a href="https://www.w3.org/TR/webauthn-3/#human-palatability">human-palatable</a> identifier for the
+    ///         <a href="https://www.w3.org/TR/webauthn-3/#relying-party">Relying Party</a>, intended only for display.
+    ///     </para>
+    ///     <para>
+    ///         This member is deprecated because many <a href="https://www.w3.org/TR/webauthn-3/#client">clients</a> do not display it, but it remains a required dictionary member for backwards compatibility.
+    ///         <a href="https://www.w3.org/TR/webauthn-3/#relying-party">Relying Parties</a> MAY, as a safe default, set this equal to the <a href="https://www.w3.org/TR/webauthn-3/#rp-id">RP ID</a>.
+    ///     </para>
+    ///     <para>
+    ///         When storing a <a href="https://www.w3.org/TR/webauthn-3/#dom-publickeycredentialentity-name">"name"</a> member’s value, the value MAY be truncated as described in <a href="https://www.w3.org/TR/webauthn-3/#sctn-strings-truncation">"String Truncation"</a> using a size
+    ///         limit greater than or equal to 64 bytes.
+    ///     </para>
     /// </summary>
     /// <remarks>
     ///     <para>DOMString</para>
-    ///     <para>
-    ///         For example:
-    ///         <list type="bullet">
-    ///             <item>
-    ///                 <description>ACME Corporation</description>
-    ///             </item>
-    ///             <item>
-    ///                 <description>Wonderful Widgets, Inc.</description>
-    ///             </item>
-    ///             <item>
-    ///                 <description>ОАО Примертех</description>
-    ///             </item>
-    ///         </list>
-    ///     </para>
-    ///     <para>Authenticators MAY truncate a name member's value so that it fits within 64 bytes, if the authenticator stores the value.</para>
     /// </remarks>
     [JsonPropertyName("name")]
     [Required]
@@ -62,15 +75,15 @@ public class PublicKeyCredentialRpEntityJSON
     public string Name { get; }
 
     /// <summary>
-    ///     A unique identifier for the <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#relying-party">Relying Party</a> entity, which sets the <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#rp-id">RP ID</a>.
+    ///     <para>A unique identifier for the <a href="https://www.w3.org/TR/webauthn-3/#relying-party">Relying Party</a> entity, which sets the <a href="https://www.w3.org/TR/webauthn-3/#rp-id">RP ID</a>.</para>
+    ///     <para>
+    ///         Specifies the <a href="https://www.w3.org/TR/webauthn-3/#rp-id">RP ID</a> the credential should be scoped to. If omitted, its value will be the <a href="https://www.w3.org/TR/credential-management-1/#credentialscontainer">CredentialsContainer</a> object’s
+    ///         <a href="https://html.spec.whatwg.org/multipage/webappapis.html#relevant-settings-object">relevant settings object’s</a> <a href="https://html.spec.whatwg.org/multipage/webappapis.html#concept-settings-object-origin">origin’s</a>
+    ///         <a href="https://html.spec.whatwg.org/multipage/browsers.html#concept-origin-effective-domain">effective domain</a>.
+    ///     </para>
     /// </summary>
     /// <remarks>
     ///     <para>DOMString</para>
-    ///     <para>
-    ///         Specifies the <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#rp-id">RP ID</a> the credential should be <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#scope">scoped</a> to. If omitted, its value will be the
-    ///         <a href="https://w3c.github.io/webappsec-credential-management/#credentialscontainer">CredentialsContainer</a> object's <a href="https://html.spec.whatwg.org/multipage/webappapis.html#relevant-settings-object">relevant settings object's</a>
-    ///         <a href="https://html.spec.whatwg.org/multipage/webappapis.html#concept-settings-object-origin">origin's</a> <a href="https://html.spec.whatwg.org/multipage/browsers.html#concept-origin-effective-domain">effective domain</a>.
-    ///     </para>
     /// </remarks>
     [JsonPropertyName("id")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
