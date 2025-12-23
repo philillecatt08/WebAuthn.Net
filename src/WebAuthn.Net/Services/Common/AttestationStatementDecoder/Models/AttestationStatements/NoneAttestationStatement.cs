d681161f0ecc25edebd17272fb9c2fb1272e1c35
Roman Bukin <r.bukin@dodobrands.io>
2025-12-23 09:18:17 +0300

    Update `NoneAttestationStatement` documentation to use stable WebAuthn specification link

--- src/WebAuthn.Net/Services/Common/AttestationStatementDecoder/Models/AttestationStatements/NoneAttestationStatement.cs
+++ src/WebAuthn.Net/Services/Common/AttestationStatementDecoder/Models/AttestationStatements/NoneAttestationStatement.cs
@@ -1,8 +1,6 @@
 ﻿namespace WebAuthn.Net.Services.Common.AttestationStatementDecoder.Models.AttestationStatements;
 
 /// <summary>
-///     Decoded <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-none-attestation">None attestation statement</a>.
+///     Decoded <a href="https://www.w3.org/TR/webauthn-3/#sctn-none-attestation">None attestation statement</a>.
 /// </summary>
-public class NoneAttestationStatement : AbstractAttestationStatement
-{
-}
+public class NoneAttestationStatement : AbstractAttestationStatement;
