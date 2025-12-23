ee13585cc1ea2b72373e8fe1379c1e01451c6a98
Roman Bukin <r.bukin@dodobrands.io>
2025-12-23 09:00:03 +0300

    Update `IAppleAnonymousAttestationStatementDecoder` documentation to use stable WebAuthn specification links and refine comments for clarity

--- src/WebAuthn.Net/Services/Common/AttestationStatementDecoder/Abstractions/AttestationStatements/IAppleAnonymousAttestationStatementDecoder.cs
+++ src/WebAuthn.Net/Services/Common/AttestationStatementDecoder/Abstractions/AttestationStatements/IAppleAnonymousAttestationStatementDecoder.cs
@@ -5,15 +5,15 @@ using WebAuthn.Net.Services.Serialization.Cbor.Models.Tree;
 namespace WebAuthn.Net.Services.Common.AttestationStatementDecoder.Abstractions.AttestationStatements;
 
 /// <summary>
-///     Decoder of <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-apple-anonymous-attestation">Apple Anonymous attestation statement</a> into a typed representation.
+///     Decoder of <a href="https://www.w3.org/TR/webauthn-3/#sctn-apple-anonymous-attestation">Apple Anonymous attestation statement</a> into a typed representation.
 /// </summary>
 public interface IAppleAnonymousAttestationStatementDecoder
 {
     /// <summary>
-    ///     Decodes <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-apple-anonymous-attestation">Apple Anonymous attestation statement</a> from <see cref="CborMap" /> into <see cref="AppleAnonymousAttestationStatement" />.
+    ///     Decodes <a href="https://www.w3.org/TR/webauthn-3/#sctn-apple-anonymous-attestation">Apple Anonymous attestation statement</a> from <see cref="CborMap" /> into <see cref="AppleAnonymousAttestationStatement" />.
     /// </summary>
     /// <param name="attStmt">
-    ///     CBOR representation of <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-apple-anonymous-attestation">Apple Anonymous attestation statement</a>.
+    ///     CBOR representation of <a href="https://www.w3.org/TR/webauthn-3/#sctn-apple-anonymous-attestation">Apple Anonymous attestation statement</a>.
     /// </param>
     /// <returns>If the decoding was successful, the result contains the <see cref="AppleAnonymousAttestationStatement" />; otherwise, the result indicates that an error occurred during the decoding process.</returns>
     Result<AppleAnonymousAttestationStatement> Decode(CborMap attStmt);
