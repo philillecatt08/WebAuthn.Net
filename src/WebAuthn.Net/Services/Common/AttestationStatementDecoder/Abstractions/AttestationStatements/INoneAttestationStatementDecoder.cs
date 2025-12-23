1e4322fe32533e9e1b891979424b6c684908a9a8
Roman Bukin <r.bukin@dodobrands.io>
2025-12-23 09:01:00 +0300

    Update `INoneAttestationStatementDecoder` documentation to use stable WebAuthn specification links and refine comments for clarity

--- src/WebAuthn.Net/Services/Common/AttestationStatementDecoder/Abstractions/AttestationStatements/INoneAttestationStatementDecoder.cs
+++ src/WebAuthn.Net/Services/Common/AttestationStatementDecoder/Abstractions/AttestationStatements/INoneAttestationStatementDecoder.cs
@@ -5,14 +5,14 @@ using WebAuthn.Net.Services.Serialization.Cbor.Models.Tree;
 namespace WebAuthn.Net.Services.Common.AttestationStatementDecoder.Abstractions.AttestationStatements;
 
 /// <summary>
-///     Decoder of <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-none-attestation">None attestation statement</a> into a typed representation.
+///     Decoder of <a href="https://www.w3.org/TR/webauthn-3/#sctn-none-attestation">None attestation statement</a> into a typed representation.
 /// </summary>
 public interface INoneAttestationStatementDecoder
 {
     /// <summary>
-    ///     Decodes <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-none-attestation">None attestation statement</a> from <see cref="CborMap" /> into <see cref="NoneAttestationStatement" />.
+    ///     Decodes <a href="https://www.w3.org/TR/webauthn-3/#sctn-none-attestation">None attestation statement</a> from <see cref="CborMap" /> into <see cref="NoneAttestationStatement" />.
     /// </summary>
-    /// <param name="attStmt">CBOR representation of <a href="https://www.w3.org/TR/2023/WD-webauthn-3-20230927/#sctn-none-attestation">None attestation statement</a>.</param>
+    /// <param name="attStmt">CBOR representation of <a href="https://www.w3.org/TR/webauthn-3/#sctn-none-attestation">None attestation statement</a>.</param>
     /// <returns>If the decoding was successful, the result contains the <see cref="NoneAttestationStatement" />; otherwise, the result indicates that an error occurred during the decoding process.</returns>
     Result<NoneAttestationStatement> Decode(CborMap attStmt);
 }
