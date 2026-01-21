f235f46d8257368a1c72b0668187f362bf7a1ba7
Roman Bukin <r.bukin@dodobrands.io>
2026-01-21 07:52:44 +0300

    Update `DisplayPNGCharacteristicsDescriptor` documentation to use stable FIDO specification links

--- src/WebAuthn.Net/Services/FidoMetadata/Models/FidoMetadataDecoder/DisplayPngCharacteristicsDescriptor.cs
+++ src/WebAuthn.Net/Services/FidoMetadata/Models/FidoMetadataDecoder/DisplayPngCharacteristicsDescriptor.cs
@@ -4,9 +4,7 @@
 ///     Display PNG Characteristics Descriptor
 /// </summary>
 /// <remarks>
-///     <para>
-///         <a href="https://fidoalliance.org/specs/mds/fido-metadata-statement-v3.0-ps-20210518.html#displaypngcharacteristicsdescriptor-dictionary">FIDO Metadata Statement - §3.8. DisplayPNGCharacteristicsDescriptor dictionary</a>
-///     </para>
+///     <a href="https://fidoalliance.org/specs/mds/fido-metadata-statement-v3.1-ps-20250521.html#sctn-type-dpngcd">FIDO Metadata Statement - DisplayPNGCharacteristicsDescriptor dictionary</a>
 /// </remarks>
 public class DisplayPngCharacteristicsDescriptor
 {
